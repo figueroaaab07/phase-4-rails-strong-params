4358909f55f839a8c9697dc9ca77f76b1ba9d9de
Alejandro Luis Figueroa Cevallos <figueroaaab07@gmail.com>
2022-08-25 13:26:42 -0700

    Completed Assignment

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -6,12 +6,6 @@ class BirdsController < ApplicationController
     render json: birds
   end
 
-  # POST /birds
-  def create
-    bird = Bird.create(name: params[:name], species: params[:species])
-    render json: bird, status: :created
-  end
-
   # GET /birds/:id
   def show
     bird = Bird.find_by(id: params[:id])
@@ -28,7 +22,6 @@ class BirdsController < ApplicationController
     # bird = Bird.create(params.permit(:name, :species))
     bird = Bird.create(bird_params)
     render json: bird, status: :created
-    byebug
   end
 
   private
